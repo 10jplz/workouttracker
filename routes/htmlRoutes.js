9b0c765dd072679a0d15ca4aa7854475fbc88a34
Jose Contreras <10jplz@Gmail.com>
2021-07-07 13:18:32 -0600

    Fixed Typo in html-routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,7 +5,7 @@ router.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 })
 router.get("/exercise", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/excercise.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 router.get("/stats", function (req, res)  {
